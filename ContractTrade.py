7c469ef7975e8aab849110c33b34a453d2cb650a
wurong <code-dream@qq.com>
2025-03-18 22:41:54 +0800

    fix

--- ContractTrade.py
+++ ContractTrade.py
@@ -6,21 +6,20 @@ def ContractTrade(exchange_name, api_key, api_secret, symbol, order_type, price,
 
 # 逐仓合约交易
 # param symbol；开多/开空；倍率；仓位（0-1）;止盈价；止损价
-def BinanceContractTrade(ex: ccxt.Exchange, symbol: str, order_type: str, price: float, amount: float, stopProfitPrice: float = None, stopLossPrice: float = None):
-    print
-    
-    if order_type == 'limit':
-        order = ex.create_order(symbol, order_type, 'buy', amount, price)
-    elif order_type == 'market':
-        order = ex.create_order(symbol, order_type, 'buy', amount)
-    else:
-        raise ValueError(f"Unsupported order type: {order_type}")
+def BinanceContractTrade(ex: ccxt.Exchange, symbol: str, side, leverage, amount: float, stopProfitPrice: float = None, stopLossPrice: float = None):
+    ex.set_margin_mode('ISOLATED', symbol, params={'leverage': leverage})
 
-    # 设置止盈和止损
+    # 如果有止盈止损价，则设置止盈止损
+    params = {}
     if stopProfitPrice is not None:
-        ex.create_order(symbol, 'limit', 'sell', amount, stopProfitPrice, {'stopPrice': stopProfitPrice})
+        params['takeProfitPrice'] = stopProfitPrice
     if stopLossPrice is not None:
-        ex.create_order(symbol, 'stop_loss_limit', 'sell', amount, stopLossPrice, {'stopPrice': stopLossPrice})
+        params['stopLossPrice'] = stopLossPrice
+    
+
+    order = ex.create_order(symbol, 'market', side, amount, None, params)
+
+    print(order)
 
 
 if __name__ == '__main__':
@@ -33,6 +32,7 @@ if __name__ == '__main__':
         'secret': my_config['api_secret'],
     })
 
-    print(ex.load_markets())
+    ex.load_markets()
+    
+    BinanceContractTrade(ex, "BTC/USDT:USDT", "buy", )
 
-    # ContractTrade(args.exchange, args.api_key, args.api_secret, args.symbol, args.order_type, args.price, args.amount)
