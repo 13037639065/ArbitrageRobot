8b80cbc666f954a3524cfa97984b3a1860d8f90a
wurong <wurong@autoxing.com>
2025-05-07 21:52:29 +0800

    xx

--- contract_orders_monitor.py
+++ contract_orders_monitor.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
             print(f"[{current_time}] 查询合约订单状态...")
             
             # 查询当前合约持有
-            open_orders = umFutures.get_orders()
+            open_orders = umFutures.query_order("*")
             print(open_orders)
             if open_orders != []:
                 # 立马下止损单
