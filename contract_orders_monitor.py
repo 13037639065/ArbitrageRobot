0b649a35611084107ae5b3271c7c5e5e8c3a16df
wurong <wurong@autoxing.com>
2025-05-07 22:01:51 +0800

    multi

--- contract_orders_monitor.py
+++ contract_orders_monitor.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
             print(f"[{current_time}] 查询合约订单状态...")
             
             # 查询当前合约持有
-            open_orders = umFutures.get_orders(symbol=".*")
+            open_orders = umFutures.get_orders(symbol="BTCUSDC:ETHUSDC")
             print(open_orders)
             if open_orders != []:
                 # 立马下止损单
